c64894c5e0e2c85ac6dd5ef81c612a41bbc2be78
iangalmiche4 <galmicheian@gmail.com>
2025-03-03 13:05:04 +0100

    first commit

---
+++ header/webpack.config.js
@@ -0,0 +1,34 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    port: 3001,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "header",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/Header",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
